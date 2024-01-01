a29878af8e6a4e12a79f61c3e83a2369e1d59309
Sybatron <vipsister2014@gmail.com>
2024-01-01 18:49:39 +0200

    Changed package name to url

--- fetch/url/url_store.go
+++ fetch/url/url_store.go
@@ -1,27 +1,27 @@
-package store
+package url
 
 import "sync"
 
-type UrlStore struct {
+type Store struct {
 	rmu  sync.RWMutex
 	urls map[string]struct{}
 }
 
-func InitUrlStore() *UrlStore {
-	return &UrlStore{
+func InitUrlStore() *Store {
+	return &Store{
 		rmu:  sync.RWMutex{},
 		urls: make(map[string]struct{}),
 	}
 }
 
-func (us *UrlStore) Contains(url string) bool {
+func (us *Store) Contains(url string) bool {
 	us.rmu.RLock()
 	defer us.rmu.RUnlock()
 	_, ok := us.urls[url]
 	return ok
 }
 
-func (us *UrlStore) Add(url string) {
+func (us *Store) Add(url string) {
 	us.rmu.Lock()
 	us.urls[url] = struct{}{}
 	us.rmu.Unlock()
