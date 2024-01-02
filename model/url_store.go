182c81ce8f895283d74bd1c70d8b82c5179c235c
Sybatron <vipsister2014@gmail.com>
2024-01-02 17:05:42 +0200

    UrlStore moved to model package

--- model/url_store.go
+++ model/url_store.go
@@ -1,27 +1,27 @@
-package url
+package model
 
 import "sync"
 
-type Store struct {
+type UrlStore struct {
 	rmu  sync.RWMutex
 	urls map[string]struct{}
 }
 
-func InitUrlStore() *Store {
-	return &Store{
+func InitUrlStore() *UrlStore {
+	return &UrlStore{
 		rmu:  sync.RWMutex{},
 		urls: make(map[string]struct{}),
 	}
 }
 
-func (us *Store) Contains(url string) bool {
+func (us *UrlStore) Contains(url string) bool {
 	us.rmu.RLock()
 	defer us.rmu.RUnlock()
 	_, ok := us.urls[url]
 	return ok
 }
 
-func (us *Store) Add(url string) {
+func (us *UrlStore) Add(url string) {
 	us.rmu.Lock()
 	us.urls[url] = struct{}{}
 	us.rmu.Unlock()
