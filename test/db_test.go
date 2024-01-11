dcfdc6d3b1aaca4b82be36e132e6439d661ec035
Sybatron <vipsister2014@gmail.com>
2024-01-11 15:33:17 +0200

    Added unit tests to test basic functionality of the app

---
+++ test/db_test.go
@@ -0,0 +1,38 @@
+package test
+
+import (
+	"fmt"
+	dbpkg "github.com/nv4n/go-crawler/fetch/db"
+	"github.com/nv4n/go-crawler/model"
+	"testing"
+	"time"
+)
+
+func TestDbConnection(t *testing.T) {
+	dbpkg.InitDb()
+	defer dbpkg.CloseDb()
+	err := dbpkg.Ping()
+	if err != nil {
+		t.Errorf("Db can't be pinged: %+v", err)
+	}
+}
+
+func ExampleImgUpload() {
+	dbpkg.InitDb()
+	defer dbpkg.CloseDb()
+	tokenStore := make(chan struct{}, 5)
+	tokenStore <- struct{}{}
+	exampleInfo := model.ImageData{Filename: "Filename", Title: "Title", AltText: "N/A", Resolution: "90x180", Format: "JPEG"}
+	dbpkg.SaveImage(exampleInfo, tokenStore)
+	time.Sleep(2 * time.Second)
+	imgs := dbpkg.GetAllImages()
+	fmt.Println(imgs)
+	affected, err := dbpkg.DeleteLastRow()
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(affected)
+	// Output:
+	// [{Filename Title N/A 90x180 JPEG}]
+	// 1
+}
