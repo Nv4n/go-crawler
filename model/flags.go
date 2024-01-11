bcc6793f7f896bedeb5fdc143f74ef9d58d565b1
Sybatron <vipsister2014@gmail.com>
2024-01-11 15:34:18 +0200

    Increased minimum goroutines possible

--- model/flags.go
+++ model/flags.go
@@ -5,6 +5,8 @@ type CliFlags struct {
 	ExternalLinks *bool
 	Spa           *bool
 	Timeout       *int  `validate:"min=1,max=10"`
-	Goroutines    *uint `validate:"min=10,max=60"`
+	Goroutines    *uint `validate:"min=15,max=100"`
 	DepthLevel    *uint `validate:"min=1,max=10"`
 }
+
+var ParsedFlags CliFlags
---
+++ model/robots_info.go
@@ -0,0 +1,8 @@
+package model
+
+import "github.com/benjaminestes/robots/v2"
+
+type RobotsInfo struct {
+	RobotsTester *robots.Robots
+	URL          string
+}
