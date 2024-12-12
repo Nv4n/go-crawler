8da88efa48bf38509078239ddaff99ca793ee9c9
Sybatron <vipsister2014@gmail.com>
2024-12-12 18:10:52 +0200

    feat(dom): updated templ image render

--- views/page_templ.go
+++ views/page_templ.go
@@ -48,12 +48,7 @@ func Page(imgs <-chan image.DbMetadata) templ.Component {
 					}()
 				}
 				ctx = templ.InitializeContext(ctx)
-				var templ_7745c5c3_Var3 string
-				templ_7745c5c3_Var3, templ_7745c5c3_Err = templ.JoinStringErrs(Image(img))
-				if templ_7745c5c3_Err != nil {
-					return templ.Error{Err: templ_7745c5c3_Err, FileName: `page.templ`, Line: 17, Col: 17}
-				}
-				_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString(templ.EscapeString(templ_7745c5c3_Var3))
+				templ_7745c5c3_Err = Image(img).Render(ctx, templ_7745c5c3_Buffer)
 				if templ_7745c5c3_Err != nil {
 					return templ_7745c5c3_Err
 				}
